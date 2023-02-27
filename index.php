621bcc980d42a2748b75d438622a27e3a439728b
nurulcse7 <nurul.cse7@gmail.com>
2023-02-27 13:04:36 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <h1>Hello World html h1</h1> -->
+    <!-- const food = 'Pizza'; -->
+    <?php 
+    /*
+    // $food ='Pizza';
+   # $Food ='Burger';
+    $Food2 ='Biriani';
+    echo 'Hello PHP 8.2';
+    echo 'I love to eat' , $Food2;
+*/
+
+    /*
+    Variable name:
+    nurulislam
+    nurulIslam // JS or php
+    nurul_islam // wp
+       
+    $age = 10;
+    $_age = 12;
+    $AGE = 14;
+    $age7 = 16;
+    // $7age = 18; // not ok (before number)
+  //  $a#ge = 19; // not ok (any special char)
+    echo 'Your age is ', $age7 . ', I read in class 13';
+    */
+
+// php addition 
+$x = 10;
+$y = 15;
+echo $x + $y . '<br>'; // 25
+echo ($x + $y) . '<br>'; // 25
+print $x + $y . '<br>'; // 25
+print ($x + $y) . '<br>'; // 25
+
+// print add value 1
+$food = 'Rice';
+print $food . '<br>'; // Rice 
+
+$food = print('Launch') . '<br>'; // Launch
+echo $food; . '<br>'; // 1 
+
+
+    ?>
+
+</body>
+</html>
